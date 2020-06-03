e29f4b8c5ff3bb0cf7e813a7dc571030c2535974
suMan <smn531@gmail.com>
2020-06-03 18:02:53 +0800

    Function added

--- function.js
+++ function.js
@@ -1,3 +1,16 @@
-function name(params) {
-    
+// Functions 
+function doubleIt(num) { 
+    let result = num * 2;
+    console.log(result);
 }
+
+doubleIt(20); // call the function with parameter
+
+// Function return
+
+function returnMe(input) {
+    let double = input * 2;
+    return double;
+}
+
+console.log(returnMe(8));
